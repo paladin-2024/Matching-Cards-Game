187e11ee2cf627c567e05c6db7c017b473d7878f
paladin-2024 <cnzabb@gmail.com>
2025-06-21 02:49:44 +0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.matching_cards_game {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.matching_cards_game to javafx.fxml;
+    exports org.example.matching_cards_game;
+}
